2a83000a5b60530ed8c9adbb0fecb1ac3c174e5b
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-17 15:21:56 +0800

    Made all scenes and made the scene transition scripts

---
+++ Assets/SceneScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void part1()
+    {
+        SceneManager.LoadScene("Part1");
+    }
+
+    public void part2()
+    {
+        SceneManager.LoadScene("Part2");
+    }
+
+    public void part3()
+    {
+        SceneManager.LoadScene("Part3");
+    }
+
+    public void part4()
+    {
+        SceneManager.LoadScene("Part4");
+    }
+
+    public void part5()
+    {
+        SceneManager.LoadScene("Part5");
+    }
+
+    public void part6()
+    {
+        SceneManager.LoadScene("Part6");
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
