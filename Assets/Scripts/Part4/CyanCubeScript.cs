fc458f879b70bf4b12c05f2da74d7892a5c5d780
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-18 11:41:24 +0800

    Part 4 completed

---
+++ Assets/Scripts/Part4/CyanCubeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyanCubeScript : MonoBehaviour
+{
+    public GameObject bluecube;
+    public GameObject bullet;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dirvec = bluecube.transform.position - this.transform.position;
+        transform.forward = dirvec;
+
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            Instantiate(bullet, this.transform.position, transform.rotation);
+            
+        }   
+    }
+}
---
+++ Assets/Scripts/Part4/Part4BlueCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part4BlueCube : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += transform.up * Mathf.Sin(Time.time * speed) * distance * Time.deltaTime;
+        this.transform.position += transform.right * Mathf.Cos(Time.time * speed) * distance * Time.deltaTime;
+    }
+}
