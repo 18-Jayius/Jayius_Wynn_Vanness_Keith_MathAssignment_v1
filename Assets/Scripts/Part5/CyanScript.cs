440326eda9335b1f901f10113576ea77f0d2ee7a
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-18 12:23:34 +0800

    Completed part 5

---
+++ Assets/Scripts/Part5/CyanScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyanScript : MonoBehaviour
+{
+    public GameObject bluecube;
+    public GameObject bullet;
+    public GameObject enterbullet;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dirvec = bluecube.transform.position - this.transform.position;
+        transform.forward = dirvec;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Instantiate(bullet, this.transform.position, transform.rotation);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(enterbullet, this.transform.position, transform.rotation);
+
+        }
+    }
+}
