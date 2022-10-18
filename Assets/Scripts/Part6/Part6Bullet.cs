324d404889bb42cd35da527248545c33ef801ea6
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-18 12:52:39 +0800

    Part 6 completed

--- Assets/Scripts/Part6/Part6Bullet.cs
+++ Assets/Scripts/Part6/Part6Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Part6Bullet : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 25f;
     public GameObject bluecube;
     
     // Start is called before the first frame update
@@ -17,12 +17,13 @@ public class Part6Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        bluecube = GameObject.FindWithTag("BlueCube");
         Vector3 dirvec = bluecube.transform.position - this.transform.position;
 
         transform.forward = dirvec.normalized;
 
-        transform.position += Vector3.MoveTowards(transform.position, bluecube.transform.position, );
+        transform.position += transform.forward * speed * Time.deltaTime;
+        Destroy(gameObject, 4f);
 
     }
       
