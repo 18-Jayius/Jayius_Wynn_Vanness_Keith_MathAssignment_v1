d6ef7b5907763d54f513475cfcb3587a35c9af04
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-18 12:43:44 +0800

    update

---
+++ Assets/Scripts/Part6/Part6Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part6Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject bluecube;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 dirvec = bluecube.transform.position - this.transform.position;
+
+        transform.forward = dirvec.normalized;
+
+        transform.position += Vector3.MoveTowards(transform.position, bluecube.transform.position, );
+
+    }
+      
+    
+}
---
+++ Assets/Scripts/Part6/Part6Cyam.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part6Cyam : MonoBehaviour
+{
+    public GameObject bluecube;
+    public GameObject bullet;
+    public GameObject enterbullet;
+    public GameObject shiftbullet;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dirvec = bluecube.transform.position - this.transform.position;
+        transform.forward = dirvec;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Instantiate(bullet, this.transform.position, transform.rotation);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(enterbullet, this.transform.position, transform.rotation);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Instantiate(shiftbullet, this.transform.position, transform.rotation);
+
+        }
+    }
+}
